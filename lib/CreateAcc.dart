b8c0913fc6d8d7a6c798cbbe4016cd48c43b3b85
LuisDiegoGR <luisdiegoticante@hotmail.com>
2024-05-07 13:35:46 -0600

    CreateCounts

--- lib/CreateAcc.dart
+++ lib/CreateAcc.dart
@@ -1,5 +1,9 @@
 import 'package:appdevfinal/pantalla_siguiente.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+enum UserRole {admin, patient}
 
 class CreateAccn extends StatefulWidget {
   const CreateAccn({super.key});
@@ -9,7 +13,40 @@ class CreateAccn extends StatefulWidget {
 }
 
 class _CreateAccnState extends State<CreateAccn> {
-  String _selectedItem = 'Admin';
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  final TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _lastNameController = TextEditingController();
+  TextEditingController _addressController = TextEditingController();
+  TextEditingController _phoneController = TextEditingController();
+
+  UserRole _selectedRole = UserRole.patient;
+
+  Future<void> _register() async {
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: _emailController.text, 
+        password: _passwordController.text
+      );
+
+      await _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'name': _nameController.text,
+        'lastName': _lastNameController.text,
+        'address': _addressController.text,
+        'phone': _phoneController.text,
+        'role': _selectedRole == UserRole.admin ? 'admin' : 'patient'
+      });
+
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => PantallaSiguiente()),
+      );
+    } catch (e) {
+      print("Error al registar usuario: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,34 +76,19 @@ class _CreateAccnState extends State<CreateAccn> {
                 ),
               ),
               SizedBox(height: 20),
-              DropdownButton(
-                value: _selectedItem,
-                onChanged: (String? newValue) {
+              DropdownButtonFormField(
+                value: _selectedRole,
+                onChanged: (value) {
                   setState(() {
-                    _selectedItem = newValue!;
+                    _selectedRole = value!;
                   });
                 },
-                items: <String>['Admin','Paciente'].map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(
-                      value,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                      ),
-                      ),
+                items: UserRole.values.map((UserRole role) {
+                  return DropdownMenuItem<UserRole>(
+                    value: role,
+                    child: Text(role == UserRole.admin ? 'admin' : 'Paciente'),
                   );
                 }).toList(),
-                icon: Icon(Icons.arrow_drop_down, color: Colors.black),
-                iconSize: 30,
-                elevation: 16,
-                style: TextStyle(color: Colors.black),
-                underline: Container(
-                  height: 2,
-                  color: Colors.black,
-                ),
-                isExpanded: true,
               ),
               const SizedBox(height: 20),
               Container(
@@ -77,7 +99,8 @@ class _CreateAccnState extends State<CreateAccn> {
                   color: const Color.fromARGB(255, 203, 203, 203),
                   borderRadius: BorderRadius.circular(10.0),
                 ),
-                child: const TextField(
+                child: TextField(
+                  controller: _emailController,
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
                     hintText: 'name@example.com',
@@ -95,7 +118,8 @@ class _CreateAccnState extends State<CreateAccn> {
                   color: const Color.fromARGB(255, 203, 203, 203),
                   borderRadius: BorderRadius.circular(10.0),
                 ),
-                child: const TextField(
+                child: TextField(
+                  controller: _passwordController,
                   keyboardType: TextInputType.visiblePassword,
                   decoration: InputDecoration(
                     hintText: 'Password',
@@ -113,7 +137,8 @@ class _CreateAccnState extends State<CreateAccn> {
                   color: const Color.fromARGB(255, 203, 203, 203),
                   borderRadius: BorderRadius.circular(10.0),
                 ),
-                child: const TextField(
+                child: TextField(
+                  controller: _nameController,
                   keyboardType: TextInputType.text,
                   decoration: InputDecoration(
                     hintText: 'Name',
@@ -131,10 +156,49 @@ class _CreateAccnState extends State<CreateAccn> {
                   color: const Color.fromARGB(255, 203, 203, 203),
                   borderRadius: BorderRadius.circular(10.0),
                 ),
-                child: const TextField(
+                child: TextField(
+                  controller: _lastNameController,
                   keyboardType: TextInputType.text,
                   decoration: InputDecoration(
-                    hintText: 'Name',
+                    hintText: 'Last Name',
+                    prefixIcon: Icon(Icons.person, color: Colors.black),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 400, // Ancho deseado del TextField
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 203, 203, 203),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: TextField(
+                  controller: _addressController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    hintText: 'Address',
+                    prefixIcon: Icon(Icons.person, color: Colors.black),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 400, // Ancho deseado del TextField
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 203, 203, 203),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: TextField(
+                  controller: _phoneController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    hintText: 'Phone Number',
                     prefixIcon: Icon(Icons.person, color: Colors.black),
                     border: InputBorder.none,
                   ),
@@ -151,6 +215,7 @@ class _CreateAccnState extends State<CreateAccn> {
                 ),
                 onPressed: () {
                   //Pacer
+                  _register();
                 },
                 child: const Text(
                   'Create',
