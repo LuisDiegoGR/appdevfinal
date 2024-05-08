1b0767b6eb343e41e0655f00d5bcffde98e8595f
AlbertoG <luis.betoxki@gmail.com>
2024-05-08 10:57:23 -0600

    changes inicioApp.dart

--- lib/InicioApp.dart
+++ lib/InicioApp.dart
@@ -4,8 +4,33 @@ import 'TerceraPage.dart';
 import 'consultar_especialista.dart';
 import 'informacion1.dart';
 
-class InicioApp extends StatelessWidget {
+class InicioApp extends StatefulWidget {
   const InicioApp({Key? key}) : super(key: key);
+
+  @override
+  _InicioAppState createState() => _InicioAppState();
+}
+
+class _InicioAppState extends State<InicioApp> {
+  bool _firstImageVisible = false;
+  bool _secondImageVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _animateImages();
+  }
+
+  void _animateImages() async {
+    await Future.delayed(Duration(milliseconds: 500)); // Esperar un momento antes de animar la primera imagen
+    setState(() {
+      _firstImageVisible = true;
+    });
+    await Future.delayed(Duration(milliseconds: 500)); // Esperar un momento antes de animar la segunda imagen
+    setState(() {
+      _secondImageVisible = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,130 +57,101 @@ class InicioApp extends StatelessWidget {
             )
           ],
         ),
-        body: Center(
+        body: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Primer contenedor con imagen delante
-              Stack(
-                children: [
-                  Container(
-                    width: 350,
-                    height: 150, // Reducimos la altura a 150
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Consultar Especialista',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    padding: EdgeInsets.only(left: 40),
-                  ),
-                  Positioned(
-                    top: 100, // Ajusta la posición vertical del botón
-                    left: 128, // Ajusta la posición horizontal del botón
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Navegar a la página ConsultarEspecialista
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => ConsultarEspecialista()),
-                        );
-                      },
-                      child: Text(
-                        'Ir',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Image.asset(
-                      'assets/images/DocGirl.png',
-                      width: 130,
-                      height: 290,
-                      alignment: Alignment.centerRight,
-                    ),
-                    padding: EdgeInsets.only(left: 26),
-                  ),
-                ],
+              SizedBox(height: 30),
+              // Primer contenedor con imagen desde el lado derecho
+              _buildAnimatedContainer(
+                context,
+                'assets/images/DocGirl.png',
+                'Consultar Especialista',
+                () {
+                  // Navegar a la página ConsultarEspecialista
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => ConsultarEspecialista()),
+                  );
+                },
+                Alignment.centerRight,
               ),
               SizedBox(height: 30),
-              // Segundo contenedor con imagen delante
-              Stack(
-                children: [
-                  Container(
-                    width: 350,
-                    height: 150, // Reducimos la altura a 150
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Informacion',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 100, // Ajusta la posición vertical del botón
-                    left: 110, // Ajusta la posición horizontal del botón
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Navegar a la página Informacion1
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => Informacion1()),
-                        );
-                      },
-                      child: Text(
-                        'Empezar',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Image.asset(
-                      'assets/images/DocMan.png',
-                      width: 130,
-                      height: 290,
-                      alignment: Alignment.centerRight,
-                    ),
-                    padding: EdgeInsets.only(left: 230),
-                  ),
-                ],
+              // Segundo contenedor con imagen desde el lado izquierdo
+              _buildAnimatedContainer(
+                context,
+                'assets/images/DocMan.png',
+                'Informacion Pediatrica',
+                () {
+                  // Navegar a la página Informacion1
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => Informacion1()),
+                  );
+                },
+                Alignment.centerLeft,
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAnimatedContainer(BuildContext context, String imagePath, String text, Function() onPressed, Alignment alignment) {
+    return TweenAnimationBuilder<double>(
+      tween: Tween(begin: 0.0, end: 1.0),
+      duration: Duration(seconds: 1),
+      curve: Curves.easeInOut,
+      builder: (context, value, child) {
+        return Transform.translate(
+          offset: Offset(alignment.x * (1 - value) * MediaQuery.of(context).size.width, 0),
+          child: child,
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto',
+              ),
+            ),
+            SizedBox(height: 20),
+            AnimatedOpacity(
+              opacity: _firstImageVisible ? 1.0 : 0.0,
+              duration: Duration(milliseconds: 500),
+              child: SizedBox(
+                height: 300,
+                width: 300,
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: onPressed,
+              child: Text(
+                'Ir',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              ),
+            ),
+          ],
         ),
       ),
     );
