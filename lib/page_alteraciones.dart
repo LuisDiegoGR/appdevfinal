28649da8a830b37c9821676fbb0264de02f876f4
LuisDiegoGR <luisdiegoticante@hotmail.com>
2024-06-05 08:17:13 -0600

    Ult

--- lib/page_alteraciones.dart
+++ lib/page_alteraciones.dart
@@ -1,3 +1,4 @@
+import 'package:appdevfinal/informacion1.dart';
 import 'package:flutter/material.dart';
 
 class PageAlteraciones extends StatelessWidget {
@@ -5,13 +6,79 @@ class PageAlteraciones extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bienvenidos a Alteraciones en el Desarrollo'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => Informacion1()),
+            );
+          },
+        ),
       ),
-      body: Center(
-        child: Text(
-          '¡Bienvenidos a la sección de Alteraciones en el Desarrollo!',
-          style: TextStyle(fontSize: 24),
-          textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'ALTERACIONES DE LA SUCCIÓN - DEGLUCIÓN',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20.0),
+              RichText(
+                textAlign: TextAlign.justify,
+                text: TextSpan(
+                  style: TextStyle(fontSize: 20),
+                  children: <TextSpan> [
+                    TextSpan(text: 'Durante la alimentación,',
+                    style: TextStyle(
+                      color: Colors.black,
+                      ),
+                    ),
+                    TextSpan(text: ' la deglución es un proceso involuntario y reflejo que está presente desde el nacimiento. Para que esta se lleve a cabo de manera adecuada, es necesaria la integración de una gran variedad de estructuras que participan en el proceso:',
+                    style: TextStyle(
+                      color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                '• Óseas.\n'
+                '• Musculares.\n'
+                '• Nerviosas.',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(height: 20.0),
+              RichText(
+                textAlign: TextAlign.justify,
+                text: TextSpan(
+                  style: TextStyle(fontSize: 20),
+                  children: <TextSpan> [
+                    TextSpan(text: 'Que canalizan las sensaciones y',
+                    style: TextStyle(
+                      color: Colors.black,
+                      ),
+                    ),
+                    TextSpan(text: ' los movimientos con la deglución. Consta de tres fases, coordinadas entre ellas y con la respiración: fase oral, fase faríngea y fase esofágica.',
+                    style: TextStyle(
+                      color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
